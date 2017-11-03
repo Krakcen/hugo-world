12f2230b7e7adc19a946eb9bd939edeb28e90be0
Hugo Villevieille <hugo.villevieille@epitech.eu>
2017-11-03 20:38:19 +0530

    -semantic +bootstrap

--- src/components/Nav/NavContent.js
+++ src/components/Nav/NavContent.js
@@ -1,8 +1,21 @@
 import React from 'react';
+import { Button, Nav, Navbar, NavItem } from 'react-bootstrap';
+
+import './Nav.css';
 
 const NavContent = () => {
     return (
-        <div>Navbar</div>
+        <Navbar>
+            <Navbar.Header>
+                <Navbar.Brand>
+                    <a href="#">React-Bootstrap</a>
+                </Navbar.Brand>
+            </Navbar.Header>
+            <Nav>
+                <NavItem eventKey={1} href="#">Link</NavItem>
+                <NavItem eventKey={2} href="#">Link</NavItem>
+            </Nav>
+        </Navbar>
     );
 };
 
